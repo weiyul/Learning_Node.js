7e2d656d0a8d38fa8238ad3e4c4f303005facbee
Wei-Yu Liao <sofialiao@hotmail.com>
2016-12-13 00:08:39 -0800

    Add files via upload

---
+++ demos/address.js
@@ -0,0 +1,12 @@
+var net = require('net');
+var server = net.createServer((socket) => {
+  socket.end('goodbye\n');
+}).on('error', (err) => {
+  // handle errors here
+  throw err;
+});
+
+// grab a random port.
+server.listen(() => {
+  console.log('opened server on', server.address());
+});
