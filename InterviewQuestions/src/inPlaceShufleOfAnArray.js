d43625b21e803cc892406ecfeaa9db380ed42305
Wei-Yu Liao <sofialiao@hotmail.com>
2017-02-02 15:11:31 -0800

    Add files via upload

---
+++ InterviewQuestions/src/inPlaceShufleOfAnArray.js
@@ -0,0 +1,5 @@
+/* 
+* Interview question and solution from Interviewcake.com
+* Q35: Write a function for doing an in-place shuffle of an array.
+
+* An in-place algorithm operates directly on its input and changes it, instead of creating and returning a new object. This is sometimes called destructive, since the original input is "destroyed" when it's edited to create the new output.
---
+++ InterviewQuestions/src/squareArray.js
@@ -0,0 +1,8 @@
+/* from interviewCake.com */
+
+/* Here are two functions that do the same operation, except one is in-place and the other is out-of-place*/
+
+
+function squareArrayInPlace(intArray){
+	return true;
+}
