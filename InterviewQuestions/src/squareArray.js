80cc1a7dd3683c2480a20b94d229bf6c7cc226bf
Wei-Yu Liao <sofialiao@hotmail.com>
2017-02-03 13:13:36 -0800

    add function squareArrayOutOfPlace

--- InterviewQuestions/src/squareArray.js
+++ InterviewQuestions/src/squareArray.js
@@ -2,14 +2,27 @@
 
 /* Here are two functions that do the same operation, except one is in-place and the other is out-of-place*/
 module.exports = function (){
+	
+	//return the input array after each element is squared
 	this.squareArrayInPlace = function (intArray){
 
-	intArray.forEach(function(currentValue, index){
+		intArray.forEach(function(currentValue, index){
 		intArray[index] *= currentValue; 
 
-	});	
+		});	
 	
-	return intArray;
+		return intArray;
+	};
+
+	//return a newly created array after each element of an input array is squared
+	this.squareArrayOutOfPlace = function(intArray){
+		var squareArray = [];
+		intArray.forEach(function(currentValue, index){
+			squareArray[index] = Math.pow(currentValue,2);
+		});
+
+		return squareArray;
+
 	};
 };
 
