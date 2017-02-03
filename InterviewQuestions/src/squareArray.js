050b874ff1fa3d7cebfa4c15d1feb67d2c272ae6
Wei-Yu Liao <sofialiao@hotmail.com>
2017-02-03 12:43:10 -0800

    function test is ready

--- InterviewQuestions/src/squareArray.js
+++ InterviewQuestions/src/squareArray.js
@@ -1,8 +1,9 @@
 /* from interviewCake.com */
 
 /* Here are two functions that do the same operation, except one is in-place and the other is out-of-place*/
-
-
-function squareArrayInPlace(intArray){
+module.exports = function (){
+	this.squareArrayInPlace = function (intArray){
 	return true;
-}
+	};
+};
+
