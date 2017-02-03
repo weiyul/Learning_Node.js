01849d0099750380eb7f248c2aa1e48bc6f4ca5a
Wei-Yu Liao <sofialiao@hotmail.com>
2017-02-03 12:50:58 -0800

    Add files via upload

--- InterviewQuestions/src/squareArray.js
+++ InterviewQuestions/src/squareArray.js
@@ -3,7 +3,8 @@
 /* Here are two functions that do the same operation, except one is in-place and the other is out-of-place*/
 module.exports = function (){
 	this.squareArrayInPlace = function (intArray){
-	return true;
+	var expectedResult = [0,1,4];	
+	return expectedResult;
 	};
 };
 
