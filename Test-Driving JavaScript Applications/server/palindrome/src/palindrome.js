d17f88124da10ea17d4e750bb3f0106b7ae4f426
Wei-Yu Liao <sofialiao@hotmail.com>
2017-02-04 17:47:48 -0800

    check undefined argument and throw error

--- Test-Driving JavaScript Applications/server/palindrome/src/palindrome.js
+++ Test-Driving JavaScript Applications/server/palindrome/src/palindrome.js
@@ -1,5 +1,7 @@
 /* code is from Test-Driving JavaScript Applications */
 
 module.exports = function(word){
+	if(word === undefined)
+		throw new Error('Invalid argument');
 	return word.trim().length>0 && word.split('').reverse().join('')===word;
 };
