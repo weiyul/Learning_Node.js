f56daf318888f09e29db2b33def51aa0998b28c5
Wei-Yu Liao <sofialiao@hotmail.com>
2017-02-08 16:32:09 -0800

    Add files via upload

--- Test-Driving JavaScript Applications/client/palindrome/test/palindrome-test.js
+++ Test-Driving JavaScript Applications/client/palindrome/test/palindrome-test.js
@@ -11,4 +11,33 @@ describe('palindrome-test', function(){
 	it('should return true for arguement mom', function(){
 		expect(isPalindrome('mom')).to.be.true;
 	});
+
+	it('should return true for arguement dad', function() {
+		expect(isPalindrome('dad')).to.be.true;
+	});
+
+	it('should return false for arguement dude', function(){
+		expect(isPalindrome('dude')).to.be.false;
+	});
+
+	it('should return true for arguement mom mom', function(){
+		expect(isPalindrome('mom mom')).to.be.true;
+	});
+
+	it('should return false for arguement mom dad', function(){
+		expect(isPalindrome('mom dad')).to.be.false;
+	})
+
+	it('should return false when arguement is an empty string', function(){
+		expect(isPalindrome('')).to.be.false;
+	});
+
+	it('should return false for argument with only two spaces', function(){
+		expect(isPalindrome('  ')).to.be.false;
+	});
+
+	it('should throw an exception if arguement is missing', function(){
+		var call = function(){isPalindrome();};
+		expect(call).to.throw(Error, 'Invalid argument');
+	});
 });
