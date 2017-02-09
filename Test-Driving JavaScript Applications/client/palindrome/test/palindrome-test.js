a068e5bea5b2478070ea7c9c20b909e6ac78b0bb
Wei-Yu Liao <sofialiao@hotmail.com>
2017-02-08 16:12:54 -0800

    isPalindrome() test

--- Test-Driving JavaScript Applications/client/palindrome/test/palindrome-test.js
+++ Test-Driving JavaScript Applications/client/palindrome/test/palindrome-test.js
@@ -1,5 +1,14 @@
+/*
+There is no require at the top of the file to bring in the isPalindrome function into scope. 
+Karma and the browser will collaborate to take care of bringing that on board
+*/
+
 describe('palindrome-test', function(){
 	it('should pass this canary test', function(){
 		expect(true).to.be.true;
+	});
+
+	it('should return true for arguement mom', function(){
+		expect(isPalindrome('mom')).to.be.true;
 	});
 });
