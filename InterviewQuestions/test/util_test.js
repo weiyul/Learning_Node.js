cc58798234fc4fb6484f65a3a1f397dd0af0f741
Wei-Yu Liao <sofialiao@hotmail.com>
2017-02-09 14:09:40 -0800

    test navieShuffle

--- InterviewQuestions/test/util_test.js
+++ InterviewQuestions/test/util_test.js
@@ -1,26 +1,30 @@
 var expect = require('chai').expect;
 var SquareArray = require('../src/squareArray.js'); //To load the file to be tested
 var ShuffleArrayInPlace = require('../src/inPlaceShufleOfAnArray.js');
-
+var squareArray, shuffleArrayInPlace;
 
 describe('util tests', function() {
+
+  beforeEach(function(){
+    squareArray = new SquareArray();
+    shuffleArrayInPlace = new ShuffleArrayInPlace();
+  });
+
   it('should pass this canary test', function() {
     expect(true).to.eql(true);
 
 
   });
 
-  var squareArray;
-  
-  beforeEach(function(){
-  	squareArray = new SquareArray();
-    shuffleArrayInPlace = new ShuffleArrayInPlace();
-  });
-
   it('should pass if the function naiveShuffle() in minimum code can work', function() {
-    var result = shuffleArrayInPlace.naiveShuffle();
-    expect(result).to.eql(true);
-    // body...
+    var theArray = [1,2];
+    var result = shuffleArrayInPlace.naiveShuffle(theArray);
+    var expectedResult = [1,2];
+    //expect(result).to.eql(true);
+    expect(result).to.eql(expectedResult);
+
+
+    
   });
 
   it('sholuld pass if the elements of array are squared by squareArrayInPlace()', function(){
