9cd162d4732d88f50bea8ff3898b0f3c921c548f
Wei-Yu Liao <sofialiao@hotmail.com>
2017-02-17 16:11:55 -0800

    mouse click to draw circles

--- LearningJavaScript/firstApplication/src/main.js
+++ LearningJavaScript/firstApplication/src/main.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	paper.install(window);
 	paper.setup(document.getElementById('mainCanvas'));
 	
+	/*//repetitive circles
 	var c;
 	for(var x=25;x<400;x+=50){
 		for(var y=25; y<400; y+=50){
@@ -10,4 +11,17 @@ $(document).ready(function(){
 			c.fillColor = 'green';
 		}
 	}
+	*/
+
+	//mounse click to draw a grid of circles 
+	var tool = new Tool();//Paper.js uses an object called a tool to handle user input
+
+	tool.onMouseDown = function(event){
+		var c = Shape.Circle(event.point.x, event.point.y, 20);
+		c.fillColor='green';
+	}
+
+
+
+
 });
