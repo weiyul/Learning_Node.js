372bf1efc4137a4b41de4eb6430b23ed81a05235
Wei-Yu Liao <sofialiao@hotmail.com>
2017-02-17 16:04:45 -0800

    repetitive circles

--- LearningJavaScript/firstApplication/src/main.js
+++ LearningJavaScript/firstApplication/src/main.js
@@ -2,9 +2,12 @@ $(document).ready(function(){
 	'use strict';
 	paper.install(window);
 	paper.setup(document.getElementById('mainCanvas'));
-	//the center of the canvas lies at (200, 200) with a radius of 50
-	var c = Shape.Circle(200,200,50);
-	c.fillColor ='green';
-	paper.view.draw();
-	console.log('main.js loaded');
+	
+	var c;
+	for(var x=25;x<400;x+=50){
+		for(var y=25; y<400; y+=50){
+			c = Shape.Circle(x,y,20);
+			c.fillColor = 'green';
+		}
+	}
 });
