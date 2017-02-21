13545723819a1efba3e1a8685e5abe4b37a9504b
Wei-Yu Liao <sofialiao@hotmail.com>
2017-02-20 17:02:36 -0800

    done parameter

--- Test-Driving JavaScript Applications/async/files/test/files-test.js
+++ Test-Driving JavaScript Applications/async/files/test/files-test.js
@@ -5,11 +5,14 @@ Code is from Test-Driving JavaScript Applications by Venkat Subramaniam
 var expect = require('chai').expect;
 var linesCount = require('../src/files');
 
+// Tests written using Mocha can include a parameter that can be used to signal the actual completion of test.
+// If a parameter is present, Mocha does not assume a test is done when it completes the test function. Instead, it waits for a signal through that parameter to declare that the test is done.
+//the parameter, done, is a way to signarl to Mocha when a test is really complete
 describe('test server-side callback', function(){
-	it('should return correct lines count for a valid file', function(){
-		//Good try, but this will not actually work, since line's count cannot be negative, but after npm test, the test is passed
+	it('should return correct lines count for a valid file', function(done){
 		var callback = function(count){
-			expect(count).to.be.eql(-2319);
+			expect(count).to.be.eql(18);
+			done();//to ensure done is called
 		};
 		linesCount('src/files.js', callback);
 	});
