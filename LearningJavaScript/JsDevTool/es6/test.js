cffc481502fa97baacf27a7efaa1839815528905
Wei-Yu Liao <sofialiao@hotmail.com>
2017-02-27 12:18:23 -0800

    Add files via upload

---
+++ LearningJavaScript/JsDevTool/es6/test.js
@@ -0,0 +1,8 @@
+'use strict';
+// es6 feature: block-scoped "let" declaration
+const sentences = [
+	{ subject : 'JavaScript', verb: 'is', object: 'great' },
+	{ subject : 'Elephants', verb: 'are', object: 'large' },
+];
+
+//es6 feature: object destructuring
