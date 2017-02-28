43b357ac8ee230025ca0f70079842b4c173bf510
Wei-Yu Liao <sofialiao@hotmail.com>
2017-02-28 15:38:40 -0800

    Numbers

--- LearningJavaScript/Literals_Variables_Constants_DataType.js
+++ LearningJavaScript/Literals_Variables_Constants_DataType.js
@@ -18,3 +18,25 @@ const roomTemp = 21.5 //decimal
 //console.log(roomTemp);
 const c = 3.0e6;//exponential (3.0 x 10^6 = 3,000,000)
 //console.log(c);//show 3000000
+const e = -1.6e-19;// exponential -1.6 x 10^-19 = 0.00000000000000000016  
+//console.log(e);//show -1.6e-19
+const inf = Infinity;
+//console.log(inf);//show Infinity
+const ninf = -Infinity;
+//console.log(ninf);//show -Infinity
+const nan = NaN; // not a number
+//console.log(nan);//show NaN
+const small = Number.EPSILON; //The smallest value that can be added to 1 to get a distinct number
+//console.log(small);//show 2.220446049250313e-16
+const bigInt = Number.MAX_SAFE_INTEGER;//The largest representable integer
+//console.log(bigInt);//show 9007199254740991
+const max = Number.MAX_VALUE;//The largest representable number
+//console.log(max);//show 1.7976931348623157e+308
+const min = Number.MIN_SAFE_INTEGER;//The smallest representable integer
+//console.log(min);//show -9007199254740991
+const nIng = Number.NEGATIVE_INFINITY;//the same as -Infinity
+//console.log(nIng);//show -Infinity
+const nan2 = Number.NaN; // the same as NaN
+//console.log(nan2);//show NaN
+const inf2 = Number.POSITIVE_INFINITY; //The same as Infinity
+console.log(inf2);//Infinity
