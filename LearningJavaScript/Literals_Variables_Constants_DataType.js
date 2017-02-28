531e09c232d78f446fb4f10ab64e89a97525f557
Wei-Yu Liao <sofialiao@hotmail.com>
2017-02-28 15:19:22 -0800

    Add files via upload

---
+++ LearningJavaScript/Literals_Variables_Constants_DataType.js
@@ -0,0 +1,20 @@
+//Variables
+let currentTempC =22;
+//console.log(currentTempC);
+let targetTempC; //equivalent to "let targetTempC = undefined"
+//console.log(targetTempC);
+let varable1, room1=" conference_room_a ", room2=" looby ";
+//console.log(varable1 + room1 + room2);
+
+//Constant
+const Room_TEMP_C =21.5, MAX_TEMP_C = 30;
+
+let count =10; //integer literal; count is still double
+const blue = 0x0000ff; //hexadecimal (hex ff = decimal 255)
+//console.log(blue);//show 255
+const umask = 0o0022;//octal (octal 22 = decimal 18)
+//console.log(umask);//show 18
+const roomTemp = 21.5 //decimal
+//console.log(roomTemp);
+const c = 3.0e6;//exponential (3.0 x 10^6 = 3,000,000)
+//console.log(c);//show 3000000
