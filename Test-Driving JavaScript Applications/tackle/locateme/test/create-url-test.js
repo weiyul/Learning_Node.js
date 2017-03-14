a6117d317269c66c10d1aa723fae1b237fe7ffe8
Wei-Yu Liao <sofialiao@hotmail.com>
2017-03-14 15:16:39 -0700

    Add files via upload

--- Test-Driving JavaScript Applications/tackle/locateme/test/create-url-test.js
+++ Test-Driving JavaScript Applications/tackle/locateme/test/create-url-test.js
@@ -8,7 +8,7 @@ describe('create-url test', function(){
 	it('should return proper url given at lat and lon', function(){
 		var latitude = -33.857;
 		var longitude = 151.215;
-		var url = createIRL(latitude, longitude);
+		var url = createURL(latitude, longitude);
 		expect(url).to.be.eql('http://maps.google.com?q=-33.857,151.215');
 	});
 });
