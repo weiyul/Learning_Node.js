337b8016a32e2466a3d1a5a0f5ac337e1c7ddaea
Wei-Yu Liao <sofialiao@hotmail.com>
2017-03-15 13:05:01 -0700

    console.log for decreasing stock prices

--- InterviewQuestions/src/maxProfitOfStock.js
+++ InterviewQuestions/src/maxProfitOfStock.js
@@ -48,6 +48,9 @@ var stockPricesYesterday =[7, 0, 1, 2, 6, 8];
 //console.log(stockPricesYesterday);
 console.log("the maximum profit is " + getMaxProfit(stockPricesYesterday));
 
+var stockPricesYesterday2 =[8,7,6,5,4,3,2,1];
+console.log("the maximum negative profit is " + getMaxProfit(stockPricesYesterday2));
+
 function getMaxProfit(stockPricesYesterday){
 
 	var maxProfit =0;
