dcfbdc1d33f57f9273f3de3ffa1e9ff2878a78fe
Wei-Yu Liao <sofialiao@hotmail.com>
2017-03-18 17:11:41 -0700

    check if sell and buy at the same time

--- InterviewQuestions/src/maxProfitOfStock.js
+++ InterviewQuestions/src/maxProfitOfStock.js
@@ -1,6 +1,6 @@
 // From https://www.interviewcake.com/question/javascript/stock-price
 // What I did is test the solution using node.js and chai
-// I also extracted some of its explanation in the comments area
+// I also add an extra codes to verify not sell at the same time, which does not exist in the original code
 
 /*
 Q1: Suppose we could access yesterday's stock prices as an array, where:
@@ -38,57 +38,27 @@ module.exports = function(){
 	this.getMaxProfit = getMaxProfit;
 }
 
+
+
+
 var stockPricesYesterday =[7, 0, 1, 2, 6, 8];
 //console.log(stockPricesYesterday);
-console.log("the maximum profit is " + getMaxProfit(stockPricesYesterday)+"for stock prices of "+stockPricesYesterday);
+console.log("the maximum profit is " 
+	+ getMaxProfit(stockPricesYesterday)
+	+" for ["+stockPricesYesterday+"]");
 
 var stockPricesYesterday2 =[8,7,6,5,4,3,2,1];
-console.log("Should be negative profit, but we have " + getMaxProfit(stockPricesYesterday2)+ " for stock prices of "+stockPricesYesterday2);
+console.log("the maximum profit is " 
+	+ getMaxProfit(stockPricesYesterday2)
+	+ " for ["
+	+stockPricesYesterday2+"]");
 
+//var stockPricesYesterday3 =[1];
+//console.log("Only one stock-price "+ getMaxProfit(stockPricesYesterday3) ); //will get Error
 
-/*
-Let's see how well we can do by looping through the array only once. 
-Since we're trying to loop through the set once, let's use a greedy approach, where we keep a
-running maxProfit until well reach the eand. We'll start ourt maxProfit at $0. As we're 
-iterating, how do we know if you've found a new maxProfit?
-
-At each iteration, our maxProfit is either:
-1. the same as the maxProfit at the last time step, or
-2. the max profit we can get by selling the the currentPrice
-
-How do we know when we have case(2)?
-The max profit we can get by selling at the currentPrice is simply the difference between the 
-currentPrice and the minPrice from earlier in the day. If this difference is greater than the
-current maxProfit, we have a new maxProfit.
-
-So for every price, we'll need to:
-1. keep track of the lowest price we've seen so far
-2. see if we can get a better profit.
-
-How can we handle negative profit?
-
-Two reasonable options could be:
-1. return a negative profit. "What's the least badly we could have done?"
-2. throw an exception. "We should not have purchased stocks yesterday!"
-
-In this case, it's probably best to go with option(1). The advantags of returning a negative
-profit are:
-
-1. We more accurately answer the challenge. If profit is "revenue minus expenses", 
-we're returning the best we could have done.
-2. It's less opinionated. We'll leave decisions up to our function's users. It would be easy
-to wrap our function in a helper function to decide if it's worth making a purchase.
-3. We allow ourselves to collect better data. It matters if we would have lost money, 
-and it matters how much we would have lost. If we're trying the get rich, we'll probably 
-care about those numbers.
-
-
-
-
-*/
 function getMaxProfit(stockPricesYesterday){
 	// If stockPricesYesterday has fewer than 2 prices
-	if(stockPricesYesterday <2){
+	if(stockPricesYesterday.length <2){
 		throw new Error('Getting a profit requires at least 2 prices');
 	}
 
@@ -96,18 +66,24 @@ function getMaxProfit(stockPricesYesterday){
 	var maxProfit = stockPricesYesterday[1]-stockPricesYesterday[0];
 
 	// go through every time
-	for(var i =0; i < stockPricesYesterday.length; i++){
+	for(var i =1; i < stockPricesYesterday.length; i++){
 		var currentPrice = stockPricesYesterday[i];
 		// ensure minPrice is the lowest price we've seen so far
 		minPrice = Math.min(minPrice, currentPrice);
 
+		//make sure the buy and sell are not at the same time
+		if(minPrice != currentPrice){
+
 		// see what our profit would be if we bought at the
 		// min price and sold at the current price
+			var potentialProfit = currentPrice - minPrice;
+			// update maxProfit if we can do better
+			maxProfit = Math.max(maxProfit, potentialProfit);
+		} 
 
-		var potentialProfit = currentPrice - minPrice;
+		
 
-		// update maxProfit if we can do better
-		maxProfit = Math.max(maxProfit, potentialProfit);
+		
 				
 	}
 	return maxProfit;
